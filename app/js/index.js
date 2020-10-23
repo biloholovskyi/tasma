91dfcdbf32b0a2d0ddcefcbabe38f35824a064bd
Dychkevych Kolya <dychkevych.kolya@gmail.com>
2020-10-23 13:11:10 +0300

    footer/news block

--- app/js/index.js
+++ app/js/index.js
@@ -3,6 +3,8 @@ import {inputForm} from "./input";
 $(document).ready(() => {
   $('input, textarea').on('input', inputForm);
 
+
+  // hover on main page -> section products
   $('.products__section--item').hover(function (){
     $(this).children('.product__button').css('opacity', '1');
     $(this).siblings().addClass('disable');
