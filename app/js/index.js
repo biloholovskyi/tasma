c0670c549010acc3b4eef798091cf2a2f446729a
Dychkevych Kolya <dychkevych.kolya@gmail.com>
2020-10-24 12:23:51 +0300

    products modal

--- app/js/index.js
+++ app/js/index.js
@@ -14,5 +14,14 @@ $(document).ready(() => {
       $(this).children('.product__button').css('opacity', '0');
       $(this).siblings().removeClass('disable');
   })
-
+// show modal products
+  $('.open__modal').on('click', function (){
+    $('.main__header .navigation ul li a').toggleClass('active');
+    $('.main__header .logo__section .lang__block p').toggleClass('active');
+    $('.main__header .logo__section .lang__block svg path').toggleClass('active');
+    $('.main__header .contact__section--tel .work__contact').toggleClass('active');
+    $('.main__header .navigation ul li  .mine').toggleClass('active');
+    $('.main__header .navigation ul li  .plus').toggleClass('active');
+    $('.modal__products').toggleClass('show');
+  });
 });
